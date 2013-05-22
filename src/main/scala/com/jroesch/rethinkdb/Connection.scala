a4e6a9d0f608438bc3962b09840e6d488241f2ed
Jared Roesch <roeschinc@gmail.com>
2013-05-22 16:35:14 -0700

    update read and write in the Connection class

--- src/main/scala/com/jroesch/rethinkdb/Connection.scala
+++ src/main/scala/com/jroesch/rethinkdb/Connection.scala
@@ -2,28 +2,41 @@ package com.jroesch.rethinkdb
 
 import java.net._
 import com.rethinkdb.QL2.VersionDummy
+import com.rethinkdb.{ QL2 => Protocol }
 import scala.language.postfixOps
+import scala.language.implicitConversions
 
 class Connection(address: String, port: Int, var db: String) {
   val socket = new Socket(address, port)
   val out = socket.getOutputStream
   val in = socket.getInputStream
 
-  writeBytes(ibytes(VersionDummy.Version.V0_1_VALUE))
+  writeBytes(VersionDummy.Version.V0_1_VALUE)
 
-  private def ibytes(i: Int): Array[Byte] = Array (
+  private implicit def intToBytes(i: Int): Array[Byte] = Array (
     (i & 0x000000FF)       toByte,
     (i & 0x0000FF00) >> 8  toByte,
     (i & 0x00FF0000) >> 16 toByte,
     (i & 0xFF000000) >> 24 toByte
   )
 
+  private implicit def bytesToInt(a: Array[Byte]): Int =
+    (a(3) << 24) + (a(2) << 16) + (a(1) << 8) + a(0)
+
   def writeBytes(bytes: Array[Byte]) { out.write(bytes) }
 
-  def writeQuery[T](query: Query[T]) {
-    val bytes = query.queryObject.toByteArray
-    writeBytes(ibytes(bytes.length))
+  def writeQuery(query: Protocol.Query) {
+    val bytes = query.toByteArray
+    writeBytes(bytes.length)
     writeBytes(bytes)
+  }
+
+  def readResponse = {
+    val size = new Array[Byte](4)
+    in.read(size, 0, 4)
+    val result = new Array[Byte](size)
+    in.read(result, 0, size)
+    result
   }
   
   //def reconnect() = {
