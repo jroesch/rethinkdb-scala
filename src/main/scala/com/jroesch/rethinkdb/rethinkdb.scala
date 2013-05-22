ce997ac2f3a5c464a8651af34e4f3610e4a941b2
Jared Roesch <roeschinc@gmail.com>
2013-05-22 16:36:57 -0700

    add listDB example to the main object

--- src/main/scala/com/jroesch/rethinkdb/rethinkdb.scala
+++ src/main/scala/com/jroesch/rethinkdb/rethinkdb.scala
@@ -1,7 +1,19 @@
 package com.jroesch
 
+import com.rethinkdb.{ QL2 => P }
+import com.jroesch.rethinkdb.Query._
+
 package object rethinkdb {
   /* extend the traits here */
   /* TODO: Top Level API here */
 
+  val testConn = new Connection("localhost", 28015, "test")
+  def listDB = {
+    val builder = new Database {}
+    val db = builder.db("test")
+    val term = builder.Term(P.Term.TermType.DB_LIST, None)
+    val query = builder.Query(term, 1, Map() + db)
+    testConn.writeQuery(query)
+    testConn.readResponse
+  }
 }
