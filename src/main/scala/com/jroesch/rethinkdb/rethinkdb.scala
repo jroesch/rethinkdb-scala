584b34bd78cd800e6fcb53414dbe3e50bd93fc47
Jared Roesch <roeschinc@gmail.com>
2013-05-23 16:00:51 -0700

    more testing changes applied to the top level object

--- src/main/scala/com/jroesch/rethinkdb/rethinkdb.scala
+++ src/main/scala/com/jroesch/rethinkdb/rethinkdb.scala
@@ -1,36 +1,39 @@
 package com.jroesch
 
-import com.rethinkdb.{ QL2 => P }
+import com.rethinkdb.{ QL2 => Protocol }
 import com.jroesch.rethinkdb.Query._
+import com.jroesch.rethinkdb.json._
 
 package object rethinkdb {
   /* extend the traits here */
   /* TODO: Top Level API here */
 
   /* JSON Trait */
-  trait JSON[A]
+  trait Datum[A]
 
-  implicit object IntJSON extends JSON[Int]
+  implicit object NoneDatum extends Datum[Option[Nothing]]
+  implicit object IntJSON extends Datum[Int]
+  implicit object DoubleJSON extends Datum[Double]
+  implicit object BoolDatum extends Datum[Boolean]
+  implicit object StringJSON extends Datum[String]
+  implicit object ArrayJSON extends Datum[Array[_]] //I'm fucked here
+  implicit object MapJSON extends Datum[Map[_, _]]
 
-  implicit object DoubleJSON extends JSON[Double]
-
-  implicit object StringJSON extends JSON[String]
-
-  implicit object ArrayJSON extends JSON[Array[_]] //I'm fucked here
-
-  implicit object MapJSON extends JSON[Map[_, _]]
-
-  val testConn = new Connection("localhost", 28015, "test")
+  implicit val testConn = new Connection("localhost", 28015, "test")
 
   def dbCreate(name: String) = new Query[Database] {
-    val query: P.Term = Term(P.Term.TermType.DB_CREATE, Some(Datum(name)))
+    val query: Protocol.Term = Term(Protocol.Term.TermType.DB_CREATE, Some(Datum(name)))
   }
 
   def dbDrop(name: String) = new Query[Database] {
-    val query = Term(P.Term.TermType.DB_DROP, Some(Datum(name)))
+    val query = Term(Protocol.Term.TermType.DB_DROP, Some(Datum(name)))
   }
 
   def dbList = new Query[Database] {
-      val query = Term(P.Term.TermType.DB_LIST, None)
+      val query = Term(Protocol.Term.TermType.DB_LIST, None)
+  }
+
+  def table(name: String, opt: JSONObject = null): Table = new Table {
+    val query = Term(Protocol.Term.TermType.TABLE, None, Datum(name) :: Nil)
   }
 }
