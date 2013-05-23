de2ea78d93b4160b44c10307a979241e45c9b4a1
Jared Roesch <roeschinc@gmail.com>
2013-05-23 16:06:24 -0700

    add readme

---
+++ Readme.md
@@ -0,0 +1,1 @@
+# A RethinkDB driver for Scala
--- src/main/scala/com/jroesch/rethinkdb/TypeOperators.scala
+++ src/main/scala/com/jroesch/rethinkdb/TypeOperators.scala
@@ -1,12 +1,12 @@
 package com.jroesch.rethinkdb
 
-/**
- * Created with IntelliJ IDEA.
- * User: jroesch
- * Date: 5/22/13
- * Time: 7:51 PM
- * To change this template use File | Settings | File Templates.
- */
-object TypeOperators {
+import language.implicitConversions
 
+object TypeOperators {
+  type Newtype[Repr, Ops] = { type Tag = NewtypeTag[Repr, Ops] }
+  trait NewtypeTag[Repr, Ops]
+
+  def newtype[Repr, Ops](r : Repr) : Newtype[Repr, Ops] = r.asInstanceOf[Any with Newtype[Repr, Ops]]
+
+  implicit def newtypeOps[Repr, Ops](t : Newtype[Repr, Ops])(implicit mkOps : Repr => Ops) : Ops = t.asInstanceOf[Repr]
 }
