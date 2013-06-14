a49b6ba42cf7ba307cb1df78dcc317bcff505dfa
Jared Roesch <roeschinc@gmail.com>
2013-06-13 20:33:51 -0700

    add insert query to Table

--- src/main/scala/com/jroesch/rethinkdb/Table.scala
+++ src/main/scala/com/jroesch/rethinkdb/Table.scala
@@ -1,17 +1,35 @@
 package com.jroesch.rethinkdb
 
+import json._
+import com.rethinkdb.{ QL2 => Protocol }
+
 abstract class Table extends Query {
   def indexCreate = ???
   def indexDrop = ???
   def indexList = ???
-  def insert = ???
+  def insert(obj: JSON)/* (opts: JSON = ) */ = query match {
+    case outerQ =>
+      val insertee = obj match {
+        case JSONObject(map) => mkObject(map)
+        case JSONArray(array) => mkArray(array)
+      }
+      new Document {
+        val query = Term(Protocol.Term.TermType.INSERT, None, outerQ :: insertee :: Nil)
+      }
+  }
+
   def update = ???
   def replace = ???
   def delete = ???
 
-  def get = ???
-  def getAll = ???
-  def between = ???
+  def get(name: String) = query match {
+    case outerQ => new Document {
+      val query = Term(Protocol.Term.TermType.GET, None, outerQ :: Nil)
+    }
+  }
+
+  def getAll(name: String, index: Option[String] = None) = ???
+  def between(lowerKey: JSON, upperKey: JSON, index: Option[String] = None) = ???
   def filter = ???
 
   def innerJoin = ???
