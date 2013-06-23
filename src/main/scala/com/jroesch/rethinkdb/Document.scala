e3ff8f5be8a1630a56864d830fddbf28e39b6369
Jared Roesch <roeschinc@gmail.com>
2013-06-22 22:00:50 -0700

    move update to Document

--- src/main/scala/com/jroesch/rethinkdb/Document.scala
+++ src/main/scala/com/jroesch/rethinkdb/Document.scala
@@ -1,6 +1,18 @@
 package com.jroesch.rethinkdb
 
+import com.jroesch.rethinkdb.json.JSON
+import com.rethinkdb.{ QL2 => Protocol }
+
 abstract class Document extends Query {
+  def update(query: JSON, update: JSON) = term match {
+    case outerQ => new Document {
+      val term = Term(Protocol.Term.TermType.UPDATE, None, outerQ :: Nil)// update.toMap
+    }
+  }
+
+  def replace = ???
+  def delete = ???
+
   def pluck = ???
   def without = ???
   def merge = ???
