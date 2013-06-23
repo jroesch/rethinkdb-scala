1accccfba71d85d81a84f7b2d313c494717cfb83
Jared Roesch <roeschinc@gmail.com>
2013-06-22 21:57:56 -0700

    add more functionality to Table
    indexCreate, indexDrop, indexList, getAll, partial implementation for between

--- src/main/scala/com/jroesch/rethinkdb/Table.scala
+++ src/main/scala/com/jroesch/rethinkdb/Table.scala
@@ -5,9 +5,27 @@ import com.rethinkdb.{ QL2 => Protocol }
 import rexp._
 
 abstract class Table extends Query with Sequence {
-  def indexCreate = ???
-  def indexDrop = ???
-  def indexList = ???
+  def indexCreate(index: String) = term match {
+    case outerQ =>
+      new Document {
+        val term = Term(Protocol.Term.TermType.INDEX_CREATE, None, List(outerQ, Datum(index)))
+      }
+  }
+
+  def indexDrop(index: JSONString) = term match {
+    case outerQ =>
+      new Document {
+        val term = Term(Protocol.Term.TermType.INDEX_DROP, None, List(outerQ, Datum(index)))
+      }
+  }
+
+  def indexList = term match {
+    case outerQ =>
+      new Document {
+        val term = Term(Protocol.Term.TermType.INDEX_LIST, None, outerQ :: Nil)
+      }
+  }
+
   def insert(obj: JSON)/* (opts: JSON = ) */ = term match {
     case outerQ =>
       val insertee = obj match {
@@ -19,18 +37,25 @@ abstract class Table extends Query with Sequence {
       }
   }
 
-  def update = ???
-  def replace = ???
-  def delete = ???
-
+  /* TODO: Options need some helps */
   def get(name: String) = term match {
     case outerQ => new Document {
       val term = Term(Protocol.Term.TermType.GET, None, outerQ :: Nil)
     }
   }
 
-  def getAll(name: String, index: Option[String] = None) = ???
-  def between(lowerKey: JSON, upperKey: JSON, index: Option[String] = None) = ???
+  def getAll(name: String, index: Option[String] = None) = term match {
+    case outerQ => new Document {
+      val term = Term(Protocol.Term.TermType.GET_ALL, None, outerQ :: Nil)
+    }
+  }
+
+  def between(lowerKey: JSON, upperKey: JSON, index: Option[String] = None) = term match {
+    case outerQ => new Document {
+      val term = null //Term(Protocol.Term.TermType.BETWEEN, )
+    }
+  }
+
   def filter = ???
 
   def innerJoin(
