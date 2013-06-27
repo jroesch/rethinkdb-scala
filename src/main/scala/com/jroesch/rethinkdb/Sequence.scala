68b626248971d0b6fd52de50360aea8ec45c8675
Jared Roesch <roeschinc@gmail.com>
2013-06-27 00:35:10 -0700

    reorganize Sequence

--- src/main/scala/com/jroesch/rethinkdb/Sequence.scala
+++ src/main/scala/com/jroesch/rethinkdb/Sequence.scala
@@ -6,13 +6,8 @@ import com.rethinkdb.{ QL2 => Protocol }
 
 trait Sequence extends Query { //with Selection {
   /* enable for comphrensions here */
-  def map = ???
-  def flatMap = concatMap
-  def concatMap = ???
-  def orderBy = ???
-  def skip = ???
-  def limit = ???
 
+  /* Selecting Data */
   def between(lowerKey: JSON, upperKey: JSON, index: Option[String] = None) = term match {
     case outerQ => new Document {
       val term = null //Term(Protocol.Term.TermType.BETWEEN, )
@@ -21,28 +16,6 @@ trait Sequence extends Query { //with Selection {
 
   def filter = ???
 
-  def apply(index: Long) = ???
-  def apply(startIndex: Long, endIndex: Long) = ???
-
-  def union(seq: Sequence): Sequence = ???
-
-  def pluck(attrs: Seq[String]): Sequence = ???
-
-  def without = ???
-
-  /* Aggregation */
-  def reduce(/* reduce function*/) = ???
-
-  def count: Double = ???
-
-  def distinct(seq: Sequence): Sequence = ???
-
-  def groupedMapReduce = ???
-
-  def groupBy = ???
-
-  def contains = ???
-
   /* Joins */
   type JoinF = (ReQLExp[RObject], ReQLExp[RObject]) => ReQLExp[RBool]
 
@@ -50,7 +23,7 @@ trait Sequence extends Query { //with Selection {
     val function = mkFunction2(pred)
     val x = term
     val y = other.term
-    new Sequence {
+    new Join {
       val term = Term(Protocol.Term.TermType.INNER_JOIN, None, List(x, y, function))
     }
   }
@@ -59,17 +32,43 @@ trait Sequence extends Query { //with Selection {
     val function = mkFunction2(pred)
     val x = term
     val y = other.term
-    new Sequence {
+    new Join {
       val term =  Term(Protocol.Term.TermType.OUTER_JOIN, None, List(x, y, function))
     }
   }
+
   def eqJoin(leftAttr: String, other: Sequence, index: Option[String] = None): Sequence = {
     val args = index match {
       case None    => List(term, term, other.term)
       case Some(i) => List(term, term, other.term, DatumTerm(i))
     }
-    new Sequence {
+    new Join {
       val term = Term(Protocol.Term.TermType.EQ_JOIN, None, args)
     }
   }
+
+  /* Tranformations */
+  def map(f: ReQLExp[RValue] => ReQLExp[RValue]): Sequence = term match {
+    case seq =>
+      val function = mkFunction1(f)
+      new Sequence {
+        val term = Term(Protocol.Term.TermType.MAP, None, List(seq, function))
+      }
+  }
+
+  def withFields(fields: JSON*) = ???
+
+  /* enable for comphrensions */
+  def flatMap = ???
+  def concatMap
+
+  def orderBy = ???
+  def skip = ???
+  def limit = ???
+
+  def apply(index: Long) = ???
+  def apply(startIndex: Long, endIndex: Long) = ???
+
+  def indexesOf = ???
+
 }
