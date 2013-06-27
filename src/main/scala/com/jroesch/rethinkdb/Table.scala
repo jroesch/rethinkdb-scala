1269250acda5c25ca785deac32e1ec30bf16989e
Jared Roesch <roeschinc@gmail.com>
2013-06-26 22:17:04 -0700

    implement joins for tables

--- src/main/scala/com/jroesch/rethinkdb/Table.scala
+++ src/main/scala/com/jroesch/rethinkdb/Table.scala
@@ -3,6 +3,7 @@ package com.jroesch.rethinkdb
 import json._
 import com.rethinkdb.{ QL2 => Protocol }
 import rexp._
+import com.rethinkdb.QL2.Term
 
 abstract class Table extends Query with Sequence {
   def indexCreate(index: String) = term match {
@@ -29,6 +30,7 @@ abstract class Table extends Query with Sequence {
   def insert(obj: JSON)/* (opts: JSON = ) */ = term match {
     case outerQ =>
       val insertee = obj match {
+
         case JSONObject(map) => mkObject(map)
         case JSONArray(array) => mkArray(array)
       }
@@ -60,8 +62,32 @@ abstract class Table extends Query with Sequence {
 
   def innerJoin(
       otherSeq: Sequence,
-      pred: (ReQLExp[RObject], ReQLExp[RObject]) => ReQLExp[RBool]): Sequence = ???
+      pred: (ReQLExp[RObject], ReQLExp[RObject]) => ReQLExp[RBool]): Sequence = {
+    val function = mkFunction2(pred)
+    val x = term
+    val y = otherSeq.term
+    new Sequence {
+      val term = Term(Protocol.Term.TermType.INNER_JOIN, None, List(x, y, function))
+    }
+  }
 
-  def outerJoin = ???
-  def eqJoin    = ???
+  def outerJoin(
+      other: Sequence,
+      pred: (ReQLExp[RObject], ReQLExp[RObject]) => ReQLExp[RBool]): Sequence = {
+    val function = mkFunction2(pred)
+    val x = term
+    val y = otherSeq.term
+    new Sequence {
+      val term: Term =  Term(Protocol.Term.TermType.OUTER_JOIN, None, List(x, y, function))
+    }
+  }
+  def eqJoin(leftAttr: String, other: Sequence, index: Option[String] = None): Sequence = {
+    val args = index match {
+      case None    => List(term, term, other.term)
+      case Some(i) => List(term, term, other.term, i)
+    }
+    new Sequence {
+      val term = Term(Protocol.Term.TermType.EQ_JOIN, None, args)
+    }
+  }
 }
